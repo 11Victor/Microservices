e26774431ac3f7e50685b1a338b6601546dc2975
11Victor <victor.valencio@qikserve.com>
2022-09-18 23:09:50 -0300

    DB CambioService - Flyway

--- 05-cambio-service/src/main/java/br/com/micro/controller/CambioController.java
+++ 05-cambio-service/src/main/java/br/com/micro/controller/CambioController.java
@@ -1,7 +1,9 @@
 package br.com.micro.controller;
 
+import br.com.micro.repository.CambioRepository;
 import java.math.BigDecimal;
 
+import java.math.RoundingMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +19,25 @@ public class CambioController {
 	
 	@Autowired
 	private Environment environment;
-	
+
+	@Autowired
+	private CambioRepository cambioRepository;
+
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable("amount") BigDecimal amount,
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
-		
+
+		var cambio = cambioRepository.findByFromAndTo(from, to);
+		if (cambio == null) throw new RuntimeException("Currency Unsupported");
+
 		var port = environment.getProperty("local.server.port");
-		
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+		BigDecimal conversionFactor = cambio.getConversionFactor();
+		BigDecimal convertedValue = conversionFactor.multiply(amount);
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+		cambio.setEnvironment(port);
+		return cambio;
 	}
 
 }
---
+++ 05-cambio-service/src/main/java/br/com/micro/repository/CambioRepository.java
@@ -0,0 +1,12 @@
+package br.com.micro.repository;
+
+import br.com.micro.model.Cambio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+    Cambio findByFromAndTo(String from, String to);
+
+}
